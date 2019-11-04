d83cb86e0d00805656bf16d6303e93b3d98753e1
wakanyan <kyoto.kobe.1126.yuto@gmail.com>
2019-11-04 23:26:22 +0900

    var2

--- passwordhacker2/ViewController.swift
+++ passwordhacker2/ViewController.swift
@@ -11,49 +11,51 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var resultLabel1: UILabel!
-       @IBOutlet var resultLabel2: UILabel!
-       @IBOutlet var resultLabel3: UILabel!
-       @IBOutlet var resultLabel4: UILabel!
-
-       @IBOutlet var countLabel: UILabel!
-       
-       var password: Int = 4390
-       
-       override func viewDidLoad() {
-           super.viewDidLoad()
-           // Do any additional setup after loading the view.
-       }
-
-       @IBAction func start(){
-           for i in 0...9999 {
-               countLabel.text = String(i)
-               RunLoop.main.run(until: Date(timeIntervalSinceNow: 0.001))
-               
-               if password == i {
-                   var digits = [Int]()
-                   for _ in 0...3{
-                       digits.append(password % 10)
-                       password = password / 10
-                   }
-                   
-                   resultLabel1.text = String(digits[0])
-                   resultLabel2.text = String(digits[1])
-                   resultLabel3.text = String(digits[2])
-                   resultLabel4.text = String(digits[3])
-               }
-           }
-       }
-       @IBAction func reset(){
-           password = 8753
-           countLabel.text = "「START」を押して解析開始"
-           resultLabel1.text = "0"
-           resultLabel2.text = "0"
-           resultLabel3.text = "0"
-           resultLabel4.text = "0"
-           
-       }
-
-   
-
+    @IBOutlet var resultLabel2: UILabel!
+    @IBOutlet var resultLabel3: UILabel!
+    @IBOutlet var resultLabel4: UILabel!
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    var password: Int = 4390
+    
+    @IBAction func start(){
+        for i in 0...9999 {
+            countLabel.text = String(i)
+            RunLoop.main.run(until: Date(timeIntervalSinceNow: 0.001))
+            
+            if password == i {
+                var digits = [Int]()
+                for _ in 0...3{
+                    digits.append(password % 10)
+                    password = password / 10
+                }
+                
+                resultLabel1.text = String(digits[0])
+                resultLabel2.text = String(digits[1])
+                resultLabel3.text = String(digits[2])
+                resultLabel4.text = String(digits[3])
+            }
+        }
+    }
+    @IBAction func reset(){
+        password = 4390
+        countLabel.text = "「START」を押して解析開始"
+        resultLabel1.text = "0"
+        resultLabel2.text = "0"
+        resultLabel3.text = "0"
+        resultLabel4.text = "0"
+        
+    }
+    
+    
+    
 }
 
